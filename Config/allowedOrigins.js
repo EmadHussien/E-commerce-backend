5737d506db89c73eaca3d6529eb5bdefeabd0a7c
Emad Hussien <emadhussien851@gmail.com>
2023-11-27 19:54:21 +0200

    Allow new endpoints

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["http://localhost:3000", "https://e-commerce-client-sand.vercel.app"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://e-commerce-client-sand.vercel.app",
+  "http://localhost:5173/",
+  "https://admin-dashboard-tan-psi.vercel.app/",
+];
 
 module.exports = allowedOrigins;
