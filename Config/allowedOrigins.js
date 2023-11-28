170a13712ba329ad06db2db06854c911d9079129
Emad Hussien <emadhussien851@gmail.com>
2023-11-28 04:34:38 +0200

    Update allowedOrigins.js

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
   "http://localhost:3000",
+  "http://localhost:5173",
   "https://e-commerce-client-sand.vercel.app",
-  "http://localhost:5173/",
-  "https://admin-dashboard-tan-psi.vercel.app/",
+  "https://admin-dashboard-tan-psi.vercel.app",
 ];
 
 module.exports = allowedOrigins;
